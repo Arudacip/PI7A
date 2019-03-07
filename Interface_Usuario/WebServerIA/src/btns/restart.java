6bb836e14d89117e662c3e3a9770d41ab6bfc50b
racass <rcassiolato@outlook.com>
2019-03-07 19:15:56 -0300

    criada calsse do botão de reinicio

---
+++ Interface_Usuario/WebServerIA/src/btns/restart.java
@@ -0,0 +1,18 @@
+package btns;
+
+import javax.swing.JButton;
+
+public class restart extends JButton
+{
+
+	/**
+	 * Esse botão será o botão de reinicio.
+	 */
+	private static final long serialVersionUID = 1L;
+	public restart()
+	{
+		this.setName("Reiniciar");
+		this.setText("Reiniciar");
+	}
+
+}
