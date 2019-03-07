f0d6df985fe005da259ad6182a96e05b92206c47
racass <rcassiolato@outlook.com>
2019-03-07 19:16:18 -0300

    criada classe do botão parar

---
+++ Interface_Usuario/WebServerIA/src/btns/stop.java
@@ -0,0 +1,18 @@
+package btns;
+
+import javax.swing.JButton;
+
+public class stop  extends JButton
+{
+
+	/**
+	 * Esse botão será o botão de parada.
+	 */
+	private static final long serialVersionUID = 1L;
+	public stop()
+	{
+		this.setName("Parar");
+		this.setText("Parar");
+	}
+
+}
