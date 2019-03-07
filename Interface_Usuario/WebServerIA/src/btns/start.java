13645c417d767b11c1511edbaf9ef59c4313766d
racass <rcassiolato@outlook.com>
2019-03-07 19:16:03 -0300

    criada classe de botão start

---
+++ Interface_Usuario/WebServerIA/src/btns/start.java
@@ -0,0 +1,18 @@
+package btns;
+
+import javax.swing.JButton;
+
+public class start extends JButton
+{
+
+	/**
+	 * Esse botão será o botão de inicio.
+	 */
+	private static final long serialVersionUID = 1L;
+	public start()
+	{
+		this.setName("Iniciar");
+		this.setText("Iniciar");
+	}
+
+}
